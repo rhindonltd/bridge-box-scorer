3655071f8339d03c8206ae5e57f885e85497db48
Jacqui Collier <jcollier@contractor.indeed.com>
2026-07-02 21:25:12 +0100

    Fix SWR key

--- src/swr/swr-keys.ts
+++ src/swr/swr-keys.ts
@@ -2,7 +2,7 @@ export const swrKeys = {
   individuals: (gameId: string) =>
     `/api/games/individual/${gameId}/participants`,
 
-  pairs: (gameId: string) => `/api/games/pairs/${gameId}/pairs`,
+  pairs: (gameId: string) => `/api/games/pairs/${gameId}/participants`,
 
   joinableGames: () => "/api/games/joinable",
 
